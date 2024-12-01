b4a7a1616f39445686f869878b1c2d99e4b99d77
Hadi Khir <khir.hadi@gmail.com>
2024-12-01 15:00:55 +0000

    updating image layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default async function Home() {
 
   const { data, error } = await supabase.from('photos').select();
-  console.log(data);
 
   return (
     <div className="w-full flex flex-col items-center py-10 px-4">
@@ -31,23 +30,23 @@ export default async function Home() {
       <div className="my-8 w-4/5 h-[1px] bg-black dark:bg-white" />
 
       {/* Image Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-4/5">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data?.map((photo) => (
           <div
             key={photo.id}
-            className="aspect-square border border-gray-300 rounded-lg flex items-center justify-center"
+            className="aspect-square border border-gray-300 rounded-lg overflow-hidden"
           >
             <Image
               src={`https://utfs.io/f/${photo.upload_key}`}
               alt={photo.caption}
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: '100%', height: 'auto' }}
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
             />
           </div>
         ))}
       </div>
+
     </div>
   );
 }
